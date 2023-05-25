6e4c1eb7e2927e76219e3e70b1fd1c8394e64458
Ingo Ruhnke <grumbel@gmail.com>
2023-05-25 14:48:17 +0200

    Update flake.nix to nixos-23.05

--- flake.nix
+++ flake.nix
@@ -2,8 +2,7 @@
   description = "A classic 2d jump'n shoot game";
 
   inputs = {
-    nixpkgs.url = "github:grumbel/nixpkgs/fix-guile-3.0";
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils";
 
     tinycmmc.url = "github:grumbel/tinycmmc";
@@ -19,26 +18,29 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
-      in rec {
-        packages = flake-utils.lib.flattenTree {
+      in {
+        packages = rec {
+          default = colorasion;
+
           colorasion = pkgs.stdenv.mkDerivation {
             pname = "colorasion";
             version = "0.2.0";
+
             src = nixpkgs.lib.cleanSource ./.;
 
             nativeBuildInputs = with pkgs; [
               cmake
               pkgconfig
             ];
+
             buildInputs = with pkgs; [
               guile_1_8
             ] ++ [
-              tinycmmc.defaultPackage.${system}
-              clanlib.defaultPackage.${system}
+              tinycmmc.packages.${system}.default
+              clanlib.packages.${system}.default
             ];
            };
         };
-        defaultPackage = packages.colorasion;
       }
     );
 }
